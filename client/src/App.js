fcc38c8ef0d44fac336e0a21133555fae8cd9d67
jinzjoseph <jinsjosephkunnummel000@gmail.com>
2024-03-23 14:00:19 +0530

    Banner component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 
 function App() {
   return (
     <div className="font-bodyFont">
       <Header/>
+      <Banner/>
 
      <Home/>
     </div>
